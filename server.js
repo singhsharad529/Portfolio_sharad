a90a6fc3a66489a89979338f4d64c55fd0329357
Sharad Kumar Singh <singhsharad529@gmail.com>
2020-12-23 12:03:41 +0530

    found the application name

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/portfolio'));
+app.use(express.static(__dirname + '/portfolio'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/portfolio/index.html'));
+res.sendFile(path.join(__dirname+'/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8090);
