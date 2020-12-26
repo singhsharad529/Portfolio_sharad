acc349c95462f74a69372f740b1c14f78a188ce2
Sharad Kumar Singh <singhsharad529@gmail.com>
2020-12-27 00:49:35 +0530

    first commit

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
+ 
 const app = express();
-
+ 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/portfolio'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/src/index.html'));
+// Replace the '/dist/<to_your_project_name>'
+app.use(express.static(__dirname + '/dist/portfolio'));
+ 
+app.get('*', function(req,res) {
+  // Replace the '/dist/<to_your_project_name>/index.html'
+  res.sendFile(path.join(__dirname + '/dist/portfolio/index.html'));
 });
-
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8090);
+app.listen(process.env.PORT || 8080);
