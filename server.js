3b640968f3cff810da92244a75e22ea783a13ac3
Sharad Kumar Singh <singhsharad529@gmail.com>
2020-12-31 01:14:06 +0530

    updates to deploy with jsonfailed on heroku

--- server.js
+++ server.js
@@ -1,16 +1,6 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('./src/assets/db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
-
-server.use(middlewares);
-server.use(router);
-
-server.listen(port);
 
 const app = express();
 
